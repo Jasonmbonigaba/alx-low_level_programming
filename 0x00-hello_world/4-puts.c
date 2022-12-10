f7db1c6e05cef2763aa24d362e92e9c957d799ce
Jasonmbonigaba <mbonigabajason20@gmail.com>
2022-12-10 07:36:51 -0800

    task3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * printing words
+ * main - Entry point
+ *
+ * return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
