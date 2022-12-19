e30b489942448ca9b29ed7e3d8dee970e0495867
Jasonmbonigaba <mbonigabajason20@gmail.com>
2022-12-19 10:39:39 -0800

    taask

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to interger parameter
+ * @n:interger pointer
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
